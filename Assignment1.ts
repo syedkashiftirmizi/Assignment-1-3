632a5c61c9fbf5925362d76e67468907cdbf7293
syedkashiftirmizi <79271879+syedkashiftirmizi@users.noreply.github.com>
2023-08-02 14:03:42 +0500

    Update Assignment1.ts

--- Assignment1.ts
+++ Assignment1.ts
@@ -13,6 +13,6 @@ console.log("Answer");
 console.log("The are many ways to see transpile code: \n\t1. Using a Code Editor Extension \n\t2.Using an Online Compiler \n\t3. Using build tools like Webpack, Rollup, or Parcel");
 /*The are many ways to see transpile code: 
     1. Using a Code Editor Extension 
-    2.Using an Online Compiler 
+    2. Using an Online Compiler 
     3. Using build tools like Webpack, Rollup, or Parcel
     */
